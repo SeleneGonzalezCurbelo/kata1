69d6b26e35b81e8d4b42a48251bedbe7767789f7
selenegc01 <selene.gonzalez106@alu.ulpgc.es>
2022-09-15 16:15:19 +0100

    1º versión de kata 1

--- src/kata1_15_09_2022/Person.java
+++ src/kata1_15_09_2022/Person.java
@@ -1,13 +1,15 @@
 package kata1_15_09_2022;
 
-import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 public class Person {
-    
-    private final String name;
-    private final Date birthdate;
 
-    public Person(String name, Date birthdate) {
+    private final String name;
+    private final Calendar birthdate;
+    private final long MILLISECONDS_PER_YEAR = (long) (1000 * 60 * 60 * 24 * 365.25);
+
+    public Person(String name, Calendar birthdate) {
         this.name = name;
         this.birthdate = birthdate;
     }
@@ -16,11 +18,16 @@ public class Person {
         return name;
     }
 
-    public Date getBirthdate() {
+    public Calendar getBirthdate() {
         return birthdate;
     }
 
-    public int getAge(){
-        return (int) ((new Date().getTime() - birthdate.getTime()) / 31557600000L);
+    public int getAge() {
+        Calendar today = GregorianCalendar.getInstance();
+        return (int) (milliSecondsToYear(today.getTimeInMillis() - birthdate.getTimeInMillis()));
+    }
+   
+    public long milliSecondsToYear(long milles) {
+        return milles / MILLISECONDS_PER_YEAR;
     }
 }
